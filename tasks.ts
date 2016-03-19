6de67023f87f3c018df35b05ab17ca361e7a595a
Cristhian Fernandez <crisfervil@gmail.com>
2016-03-19 00:19:46 +0000

    Added functionality to copy the "copy" folder to destination

--- tasks.ts
+++ tasks.ts
@@ -1,10 +1,31 @@
+/// <reference path="typings/main.d.ts" />
+
 import jigs = require("jigsaw");
 import path = require("path");
 import {sys} from "./sys";
+import ncp = require("ncp");
 
 export class BuildTasks extends jigs.TaskSet {
   @jigs.task("after-build")
   installDependencies(context:jigs.TaskExecutionContext){
+    
     sys.installDependencies(context.appDef);
+    
+    //Copy "copy"" folder
+    console.log("copying files...");
+    var source = path.join(__dirname,"copy");
+    var dest = process.cwd();
+    ncp.ncp(source,dest,error=>{
+        if(error){
+            console.log("error");
+        }
+        else{
+            console.log("done");
+        }
+    });
+    
+    // TODO: update package.json
+    // "start": "node ./bin/www"
+    
   }
 }
