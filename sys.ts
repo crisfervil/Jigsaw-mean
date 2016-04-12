e1636f246dd2204fc6f887d10d23328f970567f0
Cristhian Fernandez <crisfervil@gmail.com>
2016-04-12 23:28:21 +0100

    Added more code to a basic app

--- sys.ts
+++ sys.ts
@@ -1,38 +1,43 @@
 /// <reference path="typings/main.d.ts" />
+import fs = require("fs");
+import path = require("path");
 
-export class sys{
-  public static installDependencies(appDef){
-    console.log("installing dependencies...");
-    if(appDef&&appDef.system&&appDef.system.dependencies) {
-        var dependencies = appDef.system.dependencies;
-        for(var propName in dependencies){
-          var modName = propName, modVersion=dependencies[propName];
-          var modNameAndVersion = `${modName}@${modVersion}`;
-          console.log(modNameAndVersion);
-          if(modVersion=="link"){
-            sys.linkModule(modName);
-          }
-          else{
-            sys.installModule(modNameAndVersion);
-          }
+export class sys {
+    public static installDependencies(processDir, appDef) {
+        console.log("installing dependencies...");
+        if (appDef && appDef.system && appDef.system.dependencies) {
+            var dependencies = appDef.system.dependencies;
+            for (var propName in dependencies) {
+                // check if is already installed
+                if (!fs.existsSync(path.join(processDir, "node_modules", propName))) {
+                    var modName = propName, modVersion = dependencies[propName];
+                    var modNameAndVersion = `${modName}@${modVersion}`;
+                    console.log(modNameAndVersion);
+                    if (modVersion == "link") {
+                        sys.linkModule(modName);
+                    }
+                    else {
+                        sys.installModule(modNameAndVersion);
+                    }
+                }
+            }
         }
     }
-  }
 
-  private static linkModule(moduleName){
-    const command = ['npm','link',moduleName].join(' ');
-    const exec = require('child_process').execSync;
+    private static linkModule(moduleName) {
+        const command = ['npm', 'link', moduleName].join(' ');
+        const exec = require('child_process').execSync;
 
-    // TODO: handle errors
-    const stdout:Buffer = exec(command);
-  }
+        // TODO: handle errors
+        const stdout: Buffer = exec(command);
+    }
 
-  private static installModule(moduleNameAndVersion){
-    const command = ['npm','install',moduleNameAndVersion,'--save'].join(' ');
-    const exec = require('child_process').execSync;
+    private static installModule(moduleNameAndVersion) {
+        const command = ['npm', 'install', moduleNameAndVersion, '--save'].join(' ');
+        const exec = require('child_process').execSync;
 
-    // TODO: handle errors
-    const stdout:Buffer = exec(command);
-  }
+        // TODO: handle errors
+        const stdout: Buffer = exec(command);
+    }
 
 }
--- tasks.ts
+++ tasks.ts
@@ -6,26 +6,28 @@ import {sys} from "./sys";
 import ncp = require("ncp");
 
 export class BuildTasks extends jigs.TaskSet {
-  @jigs.task("after-build")
-  installDependencies(context:jigs.TaskExecutionContext){
-    
-    sys.installDependencies(context.appDef);
-    
-    //Copy "copy"" folder
-    console.log("copying files...");
-    var source = path.join(__dirname,"copy");
-    var dest = process.cwd();
-    ncp.ncp(source,dest,error=>{
-        if(error){
-            console.log("error");
-        }
-        else{
-            console.log("done");
-        }
-    });
-    
-    // TODO: update package.json
-    // "start": "node ./bin/www"
-    
-  }
+    @jigs.task("after-build")
+    installDependencies(context: jigs.TaskExecutionContext) {
+
+        sys.installDependencies(process.cwd(), context.appDef);
+
+        //Copy "copy"" folder
+        console.log("copying files...");
+        var source = path.join(__dirname, "copy");
+        var dest = process.cwd();
+        ncp.ncp(source, dest, error => {
+            if (error) {
+                console.log("error");
+            }
+            else {
+                console.log("done");
+            }
+        });
+
+        // TODO: create package.json if doesn't exist
+
+        // TODO: update package.json
+        // "start": "node ./bin/www"
+
+    }
 }
