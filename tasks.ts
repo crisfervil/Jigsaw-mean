37026cc65b78ee34d7f21af1afb43032d7dc31f1
Cristhian Fernandez <crisfervil@gmail.com>
2016-04-14 23:21:25 +0100

    Adding angular 2 files. TBC

--- tasks.ts
+++ tasks.ts
@@ -1,10 +1,13 @@
 import jigs = require("jigsaw");
 import path = require("path");
 import ncp = require("ncp");
+import Promise = require("promise");
 
 export class BuildTasks extends jigs.TaskSet {
     @jigs.task("after-build")
     installDependencies(context: jigs.TaskExecutionContext) {
+
+        // TODO: create a after-install task?
 
         //Copy "copy"" folder
         console.log("copying files...");
@@ -18,6 +21,16 @@ export class BuildTasks extends jigs.TaskSet {
                 console.log("done");
             }
         });
+
+        // Copy aditional files
+        var files = [{src:"node_modules/es6-shim/es6-shim.min.js",dest:"public/js/es6-shim/"},
+                     {src:"node_modules/systemjs/dist/system-polyfills.js",dest:"public/js/systemjs/"},
+                     {src:"node_modules/angular2/es6/dev/src/testing/shims_for_IE.js",dest:"public/js/angular2/"},
+                     {src:"node_modules/angular2/bundles/angular2-polyfills.js",dest:"public/js/angular2/"},
+                     {src:"node_modules/systemjs/dist/system.src.js",dest:"public/js/systemjs/"},
+                     {src:"node_modules/rxjs/bundles/Rx.js",dest:"public/js/rxjs/"},
+                     {src:"node_modules/angular2/bundles/angular2.dev.js",dest:"public/js/angular2/"},];
+
 
         // TODO: create package.json if doesn't exist
 
