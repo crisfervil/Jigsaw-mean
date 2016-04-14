75d5cc63b9160cd33d4ad72abc274dd13b713ac0
Cristhian Fernandez <crisfervil@gmail.com>
2016-04-14 22:37:31 +0100

    Small bugfix in tasks

--- tasks.ts
+++ tasks.ts
@@ -12,7 +12,7 @@ export class BuildTasks extends jigs.TaskSet {
         var dest = process.cwd();
         ncp.ncp(source, dest, error => {
             if (error) {
-                console.log("error");
+                console.log(error);
             }
             else {
                 console.log("done");
@@ -22,7 +22,7 @@ export class BuildTasks extends jigs.TaskSet {
         // TODO: create package.json if doesn't exist
 
         // TODO: update package.json
-        // "start": "node ./bin/www"
+        // add "start": "node ./bin/www"
 
     }
 }
