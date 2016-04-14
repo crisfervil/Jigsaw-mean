c69be32e820a11ef24881c2b7fe570a1c785c4eb
Cristhian Fernandez <crisfervil@gmail.com>
2016-04-14 17:20:16 +0100

    Changed the way dependencies are installed

--- tasks.ts
+++ tasks.ts
@@ -1,15 +1,10 @@
-/// <reference path="typings/main.d.ts" />
-
 import jigs = require("jigsaw");
 import path = require("path");
-import {sys} from "./sys";
 import ncp = require("ncp");
 
 export class BuildTasks extends jigs.TaskSet {
     @jigs.task("after-build")
     installDependencies(context: jigs.TaskExecutionContext) {
-
-        sys.installDependencies(process.cwd(), context.appDef);
 
         //Copy "copy"" folder
         console.log("copying files...");
